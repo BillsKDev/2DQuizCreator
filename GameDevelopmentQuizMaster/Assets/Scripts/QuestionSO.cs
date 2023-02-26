d5a9e781099de9daea70f3d95cf003d625dec9ab
BilalKhanDev1 <therealbills@hotmail.com>
2023-02-26 08:23:22 -0500

    Setting up Buttons & Scriptable Object Questions

---
+++ GameDevelopmentQuizMaster/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question text here";
+}
